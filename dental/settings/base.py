d002335fe3c30fce513e0b3b09d61979e6c31d4c
Chen Zhe <fruitschen@gmail.com>
2017-12-02 23:04:13 +0800

    初始代码，普通页面

--- dental/settings/base.py
+++ dental/settings/base.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
 
     'modelcluster',
     'taggit',
+    'compressor',
+    'django_extensions',
 
     'django.contrib.admin',
     'django.contrib.auth',
@@ -57,6 +59,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
@@ -102,15 +105,16 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-cn'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
--- home/models.py
+++ home/models.py
@@ -1,9 +1,105 @@
-from __future__ import absolute_import, unicode_literals
+# -*- coding: UTF-8 -*-
+from __future__ import unicode_literals
+from datetime import datetime
+from PIL import Image as PILImage
+from PIL import ExifTags
 
 from django.db import models
+from django.db.models.signals import post_save
+from django import forms
 
-from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.models import Page, Orderable
+from wagtail.wagtailcore.fields import RichTextField, StreamField
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel, StreamFieldPanel
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
+from wagtail.wagtailsearch import index
+from wagtail.wagtailimages.models import Image as WagtailImage
+from wagtail.wagtailcore.blocks import TextBlock, StructBlock, StreamBlock, FieldBlock, CharBlock, RichTextBlock, \
+    RawHTMLBlock, PageChooserBlock, ListBlock
+from wagtail.wagtailimages.blocks import ImageChooserBlock
+
+from modelcluster.fields import ParentalKey
+
+
+# Global Streamfield definition
+
+
+class ImageFormatChoiceBlock(FieldBlock):
+    field = forms.ChoiceField(choices=(
+        ('left', 'Wrap left'), ('right', 'Wrap right'), ('mid', 'Mid width'), ('full', 'Full width'),
+    ))
+
+
+class HTMLAlignmentChoiceBlock(FieldBlock):
+    field = forms.ChoiceField(choices=(
+        ('normal', 'Normal'), ('full', 'Full width'),
+    ))
+
+
+class PagesRichTextBlock(StructBlock):
+    content = RichTextBlock()
+
+
+class ImageBlock(StructBlock):
+    image = ImageChooserBlock()
+    caption = CharBlock(required=False, max_length=255)
+    page = PageChooserBlock(can_choose_root=True, required=False)
+
+
+class ImageListBlock(StructBlock):
+    image_list = ListBlock(ImageBlock(label="Images"))
+
+
+class PagesStreamBlock(StreamBlock):
+    paragraph = RichTextBlock(icon="pilcrow")
+    image_list = ImageListBlock(icon="image")
 
 
 class HomePage(Page):
     pass
+
+
+class SimplePage(Page):
+    intro = RichTextField(blank=True)
+    content = StreamField(
+        PagesStreamBlock,
+    )
+
+    thumbnail = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    timestamp = models.DateTimeField(
+        verbose_name=u'时间',
+        blank=True,
+        null=True
+    )
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('content'),
+    ]
+
+    api_fields = ['intro', 'content', 'thumbnail', ]
+    subpage_types = []
+
+    def save(self, *args, **kwargs):
+        result = super(SimplePage, self).save(*args, **kwargs)
+        if self.first_published_at and self.timestamp and self.first_published_at > self.timestamp:
+            self.first_published_at = self.timestamp
+            self.save()
+
+    class Meta:
+        verbose_name = u'简单页面'
+
+
+SimplePage.content_panels = [
+    FieldPanel('title', classname="full title"),
+    FieldPanel('timestamp'),
+    ImageChooserPanel('thumbnail'),
+    FieldPanel('intro', classname="full"),
+    StreamFieldPanel('content'),
+]
