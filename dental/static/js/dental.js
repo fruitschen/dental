e648646f9292b49210d0d4c7933819ecd1a718c6
Chen Zhe <fruitschen@gmail.com>
2018-05-17 00:49:58 +0800

    首页的样式调整

--- dental/static/js/dental.js
+++ dental/static/js/dental.js
@@ -1,4 +1,11 @@
 $().ready(function(){
     // the body of this function is in assets/material-kit.js
     window_width = $(window).width();
+    if(window_width <=420){
+        $('.overlap-text .initial-six').each(function(){
+            if($(this).text() == '大象家庭口腔'){
+                $(this).addClass('initial-six-bold');
+            }
+        })
+    }
 });
