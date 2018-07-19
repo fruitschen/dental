fb7313897d0008b689c57466cd34a160e57d3e7d
Chen Zhe <fruitschen@gmail.com>
2018-07-20 01:21:21 +0800

    第一个子菜单添加到Home的链接

--- home/templatetags/pages_tags.py
+++ home/templatetags/pages_tags.py
@@ -41,11 +41,12 @@ def top_menu(context, parent, calling_page=None):
 
 # Retrieves the children of the top menu items for the drop downs
 @register.inclusion_tag('pages/tags/top_menu_children.html', takes_context=True)
-def top_menu_children(context, parent):
+def top_menu_children(context, parent, is_first=False):
     menuitems_children = parent.get_children()
     menuitems_children = menuitems_children.live().in_menu()
     return {
         'parent': parent,
+        'is_first': is_first,
         'menuitems_children': menuitems_children,
         # required by the pageurl tag that we want to use within this template
         'request': context['request'],
